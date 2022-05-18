804cb5d0fd6eeaf04b8e37ec20dd51dedb4ab55f
Joe Wuthrich <joerwuthrich@gmail.com>
2022-05-19 10:17:03 +1200

    Improved score determination algorithm & confetti

--- average-color.js
+++ average-color.js
@@ -1,14 +1,22 @@
+/**
+ * An algorithm for determining how orange an image is.
+ *
+ * It takes the three most orange colors from the color palette, scores them and averages the score, compares it with the score
+ * of the dominant color and returns the best one.
+ *
+ * Credit to:
+ *  https://github.com/lokesh/color-thief
+ *  https://github.com/matkl/average-color
+ *  https://github.com/loonywizard/js-confetti
+ *  Whoever made the RGB -> LAB converter
+ */
+
 var CLIPBOARD = new CLIPBOARD_CLASS("pastebox");
-const IDEAL_LAB = xyzToLab(rgbToXyz({ r: 255, g: 145, b: 0 }));
+const IDEAL_LAB = xyzToLab(rgbToXyz({ r: 250, g: 151, b: 57 }));
 const WHITE_LAB = xyzToLab(rgbToXyz({ r: 255, g: 255, b: 255 }));
 const D65 = [95.047, 100, 108.883];
 const colorThief = new ColorThief();
-var maxParticleCount = 150; //set max confetti count
-var particleSpeed = 1; //set the particle animation speed
-var startConfetti; //call to start confetti animation
-var stopConfetti; //call to stop adding confetti
-var toggleConfetti; //call to start or stop the confetti animation depending on whether it's already running
-var removeConfetti; //call to stop the confetti animation and remove all confetti immediately
+const jsConfetti = new JSConfetti();
 
 /**
  * image pasting into canvas
@@ -63,7 +71,11 @@ function CLIPBOARD_CLASS(canvas_id) {
       "  <img />" +
       "</div>" +
       '<div class="cell color">' +
-      '  <div class="box"></div>' +
+      '  <div class="color-box">' +
+      '    <div class="box"></div>' +
+      '    <div class="box"></div>' +
+      '    <div class="box"></div>' +
+      "  </div>" +
       "  <ul>" +
       '    <li class="rgb"></li>' +
       '    <li class="score"></li>' +
@@ -72,48 +84,107 @@ function CLIPBOARD_CLASS(canvas_id) {
 
     var pastedImage = new Image();
     pastedImage.onload = function () {
-      // canvas.width = pastedImage.width;
-      // canvas.height = pastedImage.height;
+      var rgbAverage = { r: 0, g: 0, b: 0 };
+      var rgbStr;
+      var score = 0;
+      var topColorPalette = colorThief.getPalette(pastedImage, 20);
+      var dominantColor = colorThief.getColor(pastedImage, 3);
+      console.log(dominantColor);
 
-      // ctx.clearRect(0, 0, canvas.width, canvas.height);
-      // ctx.drawImage(pastedImage, 0, 0);
-      var rgbArray = colorThief.getColor(pastedImage);
-      var rgb = {};
-      rgb.r = rgbArray[0];
-      rgb.g = rgbArray[1];
-      rgb.b = rgbArray[2];
+      var scoreList = [];
+      for (var i = 0; i < topColorPalette.length; i++) {
+        var color = topColorPalette[i];
+        var score = getDeltaE(
+          IDEAL_LAB,
+          xyzToLab(rgbToXyz({ r: color[0], g: color[1], b: color[2] }))
+        );
+        scoreList.push({ index: i, score: score });
+      }
 
-      var hsl = rgbToHsl(rgb.r, rgb.g, rgb.b);
-      var rgbStr = "rgb(" + rgb.r + ", " + rgb.g + ", " + rgb.b + ")";
+      topColorPalette.sort((a, b) => {
+        return scoreList[a] > scoreList[b]
+          ? -1
+          : scoreList[a] == scoreList[b]
+          ? 0
+          : 1;
+      });
 
-      var box = element.querySelector(".box");
-      box.style.backgroundColor = rgbStr;
+      var topThreePalette = [];
+      topThreePalette.push(topColorPalette[0]);
+      topThreePalette.push(topColorPalette[1]);
+      topThreePalette.push(topColorPalette[2]);
+
+      var i = 1;
+      for (var color of topThreePalette) {
+        rgbAverage.r += color[0];
+        rgbAverage.g += color[1];
+        rgbAverage.b += color[2];
+        rgbStr = "rgb(" + color[0] + ", " + color[1] + ", " + color[2] + ")";
+
+        var box = element.children[1].children[0].children[i++ - 1];
+        box.style.backgroundColor = rgbStr;
+
+        const XYZ = rgbToXyz({ r: color[0], g: color[1], b: color[2] });
+        const LAB = xyzToLab(XYZ);
+        var singleScore = getDeltaE(IDEAL_LAB, LAB);
+        if (singleScore < 40) singleScore /= 2;
+        score += singleScore;
+      }
+
+      rgbAverage.r /= topThreePalette.length;
+      rgbAverage.g /= topThreePalette.length;
+      rgbAverage.b /= topThreePalette.length;
+      rgbStr =
+        "rgb(" +
+        Math.round(rgbAverage.r) +
+        ", " +
+        Math.round(rgbAverage.g) +
+        ", " +
+        Math.round(rgbAverage.b) +
+        ")";
+      score /= topThreePalette.length;
+
+      const XYZ = rgbToXyz({
+        r: dominantColor[0],
+        g: dominantColor[1],
+        b: dominantColor[2],
+      });
+      const LAB = xyzToLab(XYZ);
+      var dominantScore = getDeltaE(IDEAL_LAB, LAB);
+      if (dominantScore < 40) dominantScore /= 2;
+      if (score > dominantScore + 10) score = dominantScore * 1.3;
 
       // var distance = Math.sqrt(
       //   Math.pow(rgb.r - r, 2) + Math.pow(rgb.g - g, 2) + Math.pow(rgb.b - b, 2)
       // );
       // var percentage = distance / Math.sqrt(Math.pow(255, 2) * 3);
 
-      const XYZ = rgbToXyz(rgb);
-      const LAB = xyzToLab(XYZ);
-      var deltaE = getDeltaE(IDEAL_LAB, LAB);
+      // const XYZ = rgbToXyz(rgbAverage);
+      // const LAB = xyzToLab(XYZ);
+      // var deltaE = getDeltaE(IDEAL_LAB, LAB);
 
       element.querySelector(".rgb").textContent = rgbStr;
       element.querySelector(".score").textContent =
-        "Score: " + Math.round(deltaE);
+        "Score: " + Math.round(score);
       element.style.backgroundColor =
-        deltaE < 100
-          ? getColor(deltaE / 100)
+        score < 100
+          ? getColor(score / 100)
           : ["hsl(", ((1 - 100) * 120).toString(10), ",50%,75%)"].join("");
 
-      if (deltaE <= 40) {
-        startConfetti();
-        setInterval(stopConfetti, 750);
+      if (score <= 40) {
+        jsConfetti.addConfetti({
+          emojis: ["🍊", "🧡", "📙", "🦋"],
+          emojiSize: 30,
+          confettiNumber: 20,
+        });
+        jsConfetti.addConfetti();
+        // startConfetti();
+        // setInterval(stopConfetti, 750);
       }
       // element.classList.add(Math.round((1 - percentage) * 100));
     };
     pastedImage.src = source;
-    var img = element.querySelector("img");
+    var img = element.children[0].children[0];
     img.src = source;
     document.getElementById("image-wrapper").prepend(element);
   };
@@ -328,146 +399,3 @@ const calculateB = function (x1, x2) {
 
   return hab / sh;
 };
-
-(function () {
-  startConfetti = startConfettiInner;
-  stopConfetti = stopConfettiInner;
-  toggleConfetti = toggleConfettiInner;
-  removeConfetti = removeConfettiInner;
-  var colors = [
-    "DodgerBlue",
-    "OliveDrab",
-    "Gold",
-    "Pink",
-    "SlateBlue",
-    "LightBlue",
-    "Violet",
-    "PaleGreen",
-    "SteelBlue",
-    "SandyBrown",
-    "Chocolate",
-    "Crimson",
-  ];
-  var streamingConfetti = false;
-  var animationTimer = null;
-  var particles = [];
-  var waveAngle = 0;
-
-  function resetParticle(particle, width, height) {
-    particle.color = colors[(Math.random() * colors.length) | 0];
-    particle.x = Math.random() * width;
-    particle.y = Math.random() * height - height;
-    particle.diameter = Math.random() * 10 + 5;
-    particle.tilt = Math.random() * 10 - 10;
-    particle.tiltAngleIncrement = Math.random() * 0.07 + 0.05;
-    particle.tiltAngle = 0;
-    return particle;
-  }
-
-  function startConfettiInner() {
-    var width = window.innerWidth - 80;
-    var height = window.innerHeight - 60;
-    window.requestAnimFrame = (function () {
-      return (
-        window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        function (callback) {
-          return window.setTimeout(callback, 16.6666667);
-        }
-      );
-    })();
-    var canvas = document.getElementById("confetti-canvas");
-    if (canvas === null) {
-      canvas = document.createElement("canvas");
-      canvas.setAttribute("id", "confetti-canvas");
-      canvas.setAttribute(
-        "style",
-        "display:block;z-index:999999;pointer-events:none"
-      );
-      document.body.appendChild(canvas);
-      canvas.width = width;
-      canvas.height = height;
-      window.addEventListener(
-        "resize",
-        function () {
-          canvas.width = window.innerWidth;
-          canvas.height = window.innerHeight;
-        },
-        true
-      );
-    }
-    var context = canvas.getContext("2d");
-    while (particles.length < maxParticleCount)
-      particles.push(resetParticle({}, width, height));
-    streamingConfetti = true;
-    if (animationTimer === null) {
-      (function runAnimation() {
-        context.clearRect(0, 0, window.innerWidth, window.innerHeight);
-        if (particles.length === 0) animationTimer = null;
-        else {
-          updateParticles();
-          drawParticles(context);
-          animationTimer = requestAnimFrame(runAnimation);
-        }
-      })();
-    }
-  }
-
-  function stopConfettiInner() {
-    streamingConfetti = false;
-  }
-
-  function removeConfettiInner() {
-    stopConfetti();
-    particles = [];
-  }
-
-  function toggleConfettiInner() {
-    if (streamingConfetti) stopConfettiInner();
-    else startConfettiInner();
-  }
-
-  function drawParticles(context) {
-    var particle;
-    var x;
-    for (var i = 0; i < particles.length; i++) {
-      particle = particles[i];
-      context.beginPath();
-      context.lineWidth = particle.diameter;
-      context.strokeStyle = particle.color;
-      x = particle.x + particle.tilt;
-      context.moveTo(x + particle.diameter / 2, particle.y);
-      context.lineTo(x, particle.y + particle.tilt + particle.diameter / 2);
-      context.stroke();
-    }
-  }
-
-  function updateParticles() {
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    var particle;
-    waveAngle += 0.01;
-    for (var i = 0; i < particles.length; i++) {
-      particle = particles[i];
-      if (!streamingConfetti && particle.y < -15) particle.y = height + 100;
-      else {
-        particle.tiltAngle += particle.tiltAngleIncrement;
-        particle.x += Math.sin(waveAngle);
-        particle.y +=
-          (Math.cos(waveAngle) + particle.diameter + particleSpeed) * 0.5;
-        particle.tilt = Math.sin(particle.tiltAngle) * 15;
-      }
-      if (particle.x > width + 20 || particle.x < -20 || particle.y > height) {
-        if (streamingConfetti && particles.length <= maxParticleCount)
-          resetParticle(particle, width, height);
-        else {
-          particles.splice(i, 1);
-          i--;
-        }
-      }
-    }
-  }
-})();
